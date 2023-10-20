2ef9f07d14ee41f871e9ceda11b2e6e9208c439d
CarlosSimon02 <caloycamacho02@gmail.com>
2023-10-20 11:32:15 +0800

    commit 2023 october 20

--- src/habit.cpp
+++ src/habit.cpp
@@ -1,7 +1,91 @@
 #include "habit.h"
 
-habit::habit(QObject *parent)
+Habit::Habit(QObject *parent)
     : QObject{parent}
 {
 
+}
+
+int Habit::itemId() const
+{
+    return m_itemId;
+}
+
+void Habit::setItemId(int itemId)
+{
+    if (itemId != m_itemId)
+    {
+        m_itemId = itemId;
+        emit itemIdChanged();
+    }
+}
+
+QString Habit::name() const
+{
+    return m_name;
+}
+
+void Habit::setName(const QString &name)
+{
+    if (name != m_name)
+    {
+        m_name = name;
+        emit nameChanged();
+    }
+}
+
+QString Habit::category() const
+{
+    return m_category;
+}
+
+void Habit::setCategory(const QString &category)
+{
+    if (category != m_category)
+    {
+        m_category = category;
+        emit categoryChanged();
+    }
+}
+
+int Habit::frequency() const
+{
+    return m_frequency;
+}
+
+void Habit::setFrequency() const
+{
+    if (frequency != m_frequency)
+    {
+        m_frequency = frequency;
+        emit frequencyChanged();
+    }
+}
+
+QDateTime Habit::startDateTime() const
+{
+    return m_startDateTime;
+}
+
+void Habit::setStartDateTime(const QDateTime &startDateTime)
+{
+    if (startDateTime != m_startDateTime)
+    {
+        m_startDateTime = startDateTime;
+        emit startDateTimeChanged();
+    }
+}
+
+QDateTime Habit::endDateTime() const
+{
+    return m_endDateTime;
+}
+
+void Habit::setEndDateTime(const QDateTime &endDateTime)
+{
+    if (endDateTime != m_endDateTime)
+    {
+        m_endDateTime = endDateTime;
+        emit endDateTimeChanged();
+    }
 }
--- src/habit.h
+++ src/habit.h
@@ -4,29 +4,49 @@
 #include <QObject>
 #include <QtQml/qqml.h>
 
-class habit : public QObject
+class Habit : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(int itemId READ itemId WRITE setItemId NOTIFY itemIdChanged)
     Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
-    Q_PROPERTY(QString imageSource READ imageSource WRITE setImageSource NOTIFY imageSourceChanged)
     Q_PROPERTY(QString category READ category WRITE setCategory NOTIFY categoryChanged)
+    Q_PROPERTY(QString frequency READ frequency WRITE setFrequency NOTIFY frequencyChanged)
     Q_PROPERTY(QDateTime startDateTime READ startDateTime WRITE setStartDateTime NOTIFY startDateTimeChanged)
     Q_PROPERTY(QDateTime endDateTime READ endDateTime WRITE setEndDateTime NOTIFY endDateTimeChanged)
-    Q_PROPERTY(int progressTracker READ progressTracker WRITE setProgressTracker NOTIFY progressTrackerChanged)
-    Q_PROPERTY(QString progressUnit READ progressUnit WRITE setProgressUnit NOTIFY progressUnitChanged)
-    Q_PROPERTY(QString mission READ mission WRITE setMission NOTIFY missionChanged)
-    Q_PROPERTY(QString vision READ vision WRITE setVision NOTIFY visionChanged)
-    Q_PROPERTY(QString obstacles READ obstacles WRITE setObstacles NOTIFY obstaclesChanged)
-    Q_PROPERTY(QString resources READ resources WRITE setResources NOTIFY resourcesChanged)
     Q_PROPERTY(int parentGoalId READ parentGoalId WRITE setParentGoalId NOTIFY parentGoalIdChanged)
     QML_ELEMENT
 
 public:
-    explicit habit(QObject *parent = nullptr);
+    explicit Habit(QObject *parent = nullptr);
+
+    int itemId() const;
+    void setItemId(int itemId);
+    QString name() const;
+    void setName(const QString& name);
+    QString category() const;
+    void setCategory(const QString& category);
+    int frequency() const;
+    void setFrequency() const;
+    QDateTime startDateTime() const;
+    void setStartDateTime(const QDateTime& startDateTime);
+    QDateTime endDateTime() const;
+    void setEndDateTime(const QDateTime& endDateTime);
 
 signals:
+    void itemIdChanged();
+    void nameChanged();
+    void categoryChanged();
+    void frequencyChanged();
+    void startDateTimeChanged();
+    void endDateTimeChanged();
 
+private:
+    int m_itemId;
+    QString m_name;
+    QString m_category;
+    int m_frequency;
+    QDateTime m_startDateTime;
+    QDateTime m_endDateTime;
 };
 
 #endif // HABIT_H
